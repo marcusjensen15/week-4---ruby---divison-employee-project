fdfff8af7799a939e69625b1a18d767cdb209b24
Student <example@example.com>
2020-01-16 09:23:54 -0800

    seeder now works with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#this seeder works
+
+50.times do |index|
+
+  Division.create!(name: Faker::Food.spice)
+end
+
+p "Created #{Division.count} divisions"
+
+
+
+Employee.destroy_all
+
+
+
+50.times do |index|
+
+  Employee.create!(employee_name: Faker::Food.spice,
+                        division_id: rand(50))
+end
+
+p "Created #{Employee.count} employees"
+
+
+50.times do |index|
+
+  Project.create!(project_name: Faker::Food.spice)
+end
+
+p "Created #{Project.count} Projects"
